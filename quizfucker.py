aa30d0dd6f54a8e6545628c2993173932750d3fa
yuejiancc <yuban747@gmail.com>
2022-10-23 23:41:16 +0800

    pick up the answer

--- quizfucker.py
+++ quizfucker.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import ddddocr
+
 input_question = input("完整的输入问题： ")
 
 # convert input_questions to raw_questions
@@ -22,9 +24,28 @@ def get_answer_url(raw_question):
     answer_url = "https://yhxt.liantibao.com" + handled_tag
     return answer_url
 
-
 answer_url = get_answer_url(raw_question)
 
-print(answer_url)
+# get the answer
+def get_answer(answer_url):
+    url = answer_url
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "lxml")
+    tag = str(soup.body.find_all(attrs={"class": "detail-answer"}))
+    answer_pic_url = tag.split("src=")[1].split("\"")[1]
+
+    ocr = ddddocr.DdddOcr()
+    # pick up the answer using ddddocr
+    with open("answer_pic.png", "wb+") as f:
+        f.write(requests.get(answer_pic_url).content)
+
+    img_bytes = open("answer_pic.png", "rb").read()
+    answer = ocr.classification(img_bytes)
+
+    return answer
+
+answer = get_answer(answer_url).upper
+
+print(answer)
 
 # To be continued…
